83256aced161cb24815819abae5bd6cbdb1fb322
Arij Chabbouh <areejchabbouh.jy@gmail.com>
2026-03-28 16:14:47 +0100

    Initial commit

---
+++ src/main/java/com/pfa/gestion_pfa/GestionPfaApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.gestion_pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa/gestion_pfa/GestionPfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.gestion_pfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
